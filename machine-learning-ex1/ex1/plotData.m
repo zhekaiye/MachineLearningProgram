d56a43fc92a3037a63f3e83dc6d62666aa901f7c
zhekaiye <yzk2014new@126.com>
2019-09-03 12:35:59 +0800

    finish ex1

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -3,7 +3,11 @@ function plotData(x, y)
 %   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
 %   population and profit.
 
-figure; % open a new figure window
+% figure; % open a new figure window
+
+plot(x, y, 'rx', 'MarkerSize', 10);
+ylabel('Profit in $10,000s');
+xlabel('Population of City in 10,000s');
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the training data into a figure using the 
